c38580614e9cef07cb4b1b3c5b35df0a195bff95
raviMukti <shinjiravi@gmail.com>
2021-08-26 11:24:04 +0700

    Adding Unit Test for ShopeeClient

---
+++ tests/client/ShopeeClientTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\client;
+
+use Haistar\ShopeePhpSdk\client\ShopeeClient;
+use PHPUnit\Framework\TestCase;
+
+class ShopeeClientTest extends TestCase
+{
+    public function testCreateShopeeClient(){
+        $client = new ShopeeClient();
+        $client->setPartnerId(001);
+
+        $this->assertEquals($client->getPartnerId(), 001);
+    }
+}
